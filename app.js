2faa2bac813f88b54ea14639c0d4b459153567bb
muhammedrishadpe <rishadpe@gmail.com>
2021-06-16 03:28:46 +0530

    added js

--- app.js
+++ app.js
@@ -5,10 +5,28 @@ function animatedForm(){
         arrow.addEventListener('click', () => {
             const input = arrow.previousElementSibling;
             const parent = arrow.parentElement;
-            console.log(input);
+            const nextForm = parent.nextElementSibling;
+
+            //check for validation
+            if(input.type === "text" && validateUser(input)){
+                console.log("everything is okey!");
+            }
         });
     });
 }
 
+function validateUser(user){
+    if(user.value.length < 6){
+         console.log('not enough characters');
+         error('rgb(189,87,87');
+    }else{
+        error("rgb(87, 189, 130)");
+        return true; 
+    }
+}
+
+function error(color) {
+    document.body.style.backgroundColor = color;
+}
 
 animatedForm();
