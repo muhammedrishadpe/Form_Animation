755e2e0b1b6a118968e2df868d40fb3d083101f7
muhammedrishadpe <rishadpe@gmail.com>
2021-06-16 03:04:14 +0530

    updated css

---
+++ app.js
@@ -0,0 +1,14 @@
+function animatedForm(){
+    const arrows = document.querySelectorAll('.fa-arrow-down');
+
+    arrows.forEach(arrow => {
+        arrow.addEventListener('click', () => {
+            const input = arrow.previousElementSibling;
+            const parent = arrow.parentElement;
+            console.log(input);
+        });
+    });
+}
+
+
+animatedForm();
